dc6443bc75515fd99ed31ad034ea3f5d56c3b071
SoraK93 <SoraK93@users.noreply.github.com>
2026-01-29 06:13:33 +0530

    control flow lesson complete

--- operator_controlFlow/main.go
+++ operator_controlFlow/main.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("\nComparison Operator")
 	comparison()
 
+	fmt.Println("\nArithematic Operator")
+	arithematic()
+
+	fmt.Println("\nLogical Operator")
+	logical()
+
+	fmt.Println("\nBitwise Operator")
+	bitwise()
+
+	fmt.Println("\nIf-else statement")
+	ifElse()
+
+	fmt.Println("\nSwitch statement")
+	switchStatement()
+
+	fmt.Println("\nLoop statement")
+	loop()
 }
 
 func comparison() {
@@ -24,9 +42,79 @@ func comparison() {
 
 func arithematic() {
 	var (
-		a float64 = 5.9
-		b, c, d int = 2, 10, 4
+		a       float64 = 5.9
+		b, c, d int     = 2, 10, 4
 	)
 
 	fmt.Println((int(a) + b) * c / d)
+}
+
+func logical() {
+	var a bool = true
+	result := 10 > 50
+	fmt.Println(!(a || result))
+}
+
+func bitwise() {
+	var x, y int = 100, 90
+	fmt.Println(x & y)
+	fmt.Println(x | y)
+	fmt.Println((!(((x + y) >> 2) == 47)))
+}
+
+func ifElse() {
+	var a, b string = "foo", "bar"
+	if a+b == "foo" {
+		fmt.Println("foo")
+	} else if a+b == "foobar" {
+		fmt.Println("bar")
+	} else if a+b == "bar" {
+		fmt.Println("foobar")
+	} else {
+		fmt.Println("None matched")
+	}
+	fmt.Println("thank you!")
+
+}
+
+func switchStatement() {
+	var i, j = 10, 50
+	switch {
+	case i+j == 60:
+		fmt.Println("equal to 60")
+	case i+j <= 60:
+		fmt.Println("less than or equal to 60")
+		fallthrough
+	default:
+		fmt.Println("greater than 60")
+	}
+
+	day := "wednesday"
+	switch day {
+	case "monday":
+		fmt.Println("monday")
+	case "tuesday":
+		fmt.Println("tuesday")
+	case "wednesday":
+		fmt.Println("wednesday")
+		fallthrough
+	case "thursday":
+		fmt.Println("thursday")
+		fallthrough
+	case "friday":
+		fmt.Println("friday")
+	case "saturday", "sunday":
+		fmt.Println("weekend")
+	default:
+		fmt.Println("default")
+	}
+}
+
+func loop() {
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i * i)
+		if i == 3 {
+			break
+		}
+	}
 }
